4a8d0ce355e002b2d260aec029ef119519b0efef
Nnaemeka Nnodim <mannynnodim@gmail.com>
2024-01-31 16:53:19 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
+import OurClasses from "@/scenes/ourClasses";
+import ContactUs from "@/scenes/contactUs";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "./shared/types";
+import Footer from "@/scenes/footer";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home,
+  );
+  const [isPageTop, setIsPageTop] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsPageTop(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) {
+        setIsPageTop(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <>
+      <div className="app bg-gray-20">
+        <Navbar
+          isPageTop={isPageTop}
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
+        <Home setSelectedPage={setSelectedPage} />
+        <Benefits setSelectedPage={setSelectedPage} />
+        <OurClasses setSelectedPage={setSelectedPage} />
+        <ContactUs setSelectedPage={setSelectedPage} />
+        <Footer/>
+      </div>
+    </>
+  );
+}
+
+export default App;
